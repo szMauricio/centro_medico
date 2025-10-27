a6e8bb46ec90d9e7aed25f23b14fe2629e086fd0
szMauricio <szmauricio02@gmail.com>
2025-10-27 18:53:03 -0300

    Configuración inicial

---
+++ src/main/java/com/porfolio/centro_medico/CentroMedicoApplication.java
@@ -0,0 +1,13 @@
+package com.porfolio.centro_medico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentroMedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentroMedicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porfolio/centro_medico/CentroMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.centro_medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
